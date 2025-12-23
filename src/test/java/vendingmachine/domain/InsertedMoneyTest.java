732220fcacfd8c390ff7bf7e267759f9f32a50a2
khcho96 <khcho1492@gmail.com>
2025-12-23 09:46:50 +0900

    test: 단위테스트 작성

---
+++ src/test/java/vendingmachine/domain/InsertedMoneyTest.java
@@ -0,0 +1,17 @@
+package vendingmachine.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import vendingmachine.constant.ErrorMessage;
+
+class InsertedMoneyTest {
+
+    @Test
+    void 투입_금액_10원_단위_아니면_예외_발생() {
+        assertThatThrownBy(() -> InsertedMoney.fromAmount(123))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining(ErrorMessage.AMOUNT_UNIT_ERROR.getErrorMessage());
+    }
+}
